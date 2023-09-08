f2a136e87c8e90b50c97cf6ad28a7672af929651
Sirlibola <asher6421@gmail.com>
2023-09-08 16:37:30 +0500

    data

--- app/src/main/java/uz/abboskhan/firebaserealtime/CategoryAdapter.kt
+++ app/src/main/java/uz/abboskhan/firebaserealtime/CategoryAdapter.kt
@@ -1,37 +1,44 @@
 package uz.abboskhan.firebaserealtime
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import uz.abboskhan.firebaserealtime.databinding.RewItemBinding
 
-class CategoryAdapter(private var mList: List<CountryData>) :
-        RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
+class CategoryAdapter(private var mList: List<CountryData>, val c: Context) :
+    RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
 
-        inner class CategoryViewHolder(private var binding: RewItemBinding) :
-            RecyclerView.ViewHolder(binding.root) {
+    inner class CategoryViewHolder(private var binding: RewItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
-            fun bing(data: CountryData) {
+        fun bing(data: CountryData) {
 
-                Picasso.get().load(data.imageUrl).into(binding.itemImg)
+            Picasso.get().load(data.imageUrl).into(binding.itemImg)
 
-                binding.itemTxt.text = data.country
+            binding.itemTxt.text = data.country
 
 
-
-            }
         }
+    }
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
-            val binding = RewItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-            return CategoryViewHolder(binding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        val binding = RewItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CategoryViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        val currentItem = mList[position]
+        val country = currentItem.country
+        holder.bing(currentItem)
+        holder.itemView.setOnClickListener {
+            val i  = Intent(c, MainActivity::class.java)
+            i.putExtra("country",country)
+            c.startActivity(i)
+
         }
-
-        override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
-            val currentItem = mList[position]
-            holder.bing(currentItem)
-
 
 //        with(holder.binding) {
 //            with(mList[position]) {
@@ -39,11 +46,11 @@ class CategoryAdapter(private var mList: List<CountryData>) :
 //            }
 //        }
 
-        }
-
-        override fun getItemCount(): Int {
-            return mList.size
-        }
-
-
     }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+
+}
--- app/src/main/java/uz/abboskhan/firebaserealtime/CountryData.kt
+++ app/src/main/java/uz/abboskhan/firebaserealtime/CountryData.kt
@@ -1,3 +1,5 @@
 package uz.abboskhan.firebaserealtime
 
 data class CountryData(val imageUrl: String = "", val country: String = "")
+    //capital , field, population,info
+    //
